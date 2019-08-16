b01ddba9ca8d3fca02ac1b4d9edc4afb5d86b4fa
Judith Cristina <judicriss18@gmail.com>
2019-08-16 03:46:48 -0500

    modificando el css de los likes

--- src/view/Wall.js
+++ src/view/Wall.js
@@ -21,7 +21,7 @@ export const home = (posts) => {
         </div>
         <div class="container-user">
             ${user.photoURL === null ? `<img class="img-user" src="../assets/perfil-email.jpg"/>` : `<img class="img-user" src="${user.photoURL}"/>`}
-            ${user.displayName === null ? `<p id="inf-user"><strong> ${user.email}</strong><p>`:`<p id="inf-user"><strong>${user.displayName}</strong><p>` }  
+            ${user.displayName === null ? `<p id="inf-user" class="margin-name"><strong> ${user.email}</strong><p>`:`<p id="inf-user"  class="margin-name"><strong>${user.displayName}</strong><p>` }  
         </div>
 	</section>
 	<section class="posts">
@@ -123,9 +123,9 @@ export const postListTemplate = (postObject) => {
 				<div class="post-content clear">
 				  <textarea id="post-edit-${postObject.id}" class="border-box post-article textarea border-none" readOnly=true>${postObject.content}</textarea>
 				  ${(postObject.image !== undefined && postObject.image !== null) ? `<img class="image-post" src="${postObject.image}" alt="post-image" title="post image" />` : ``}
-				</div>
-        		<div class="post-article bg-light-green post-footer border-box">
 				  <span id="likes-count-${postObject.id}" class="color-black registry">${postObject.likes}</span>
+				  </div>
+        		<div class="post-article bg-light-green post-footer border-box">
 				  <img id="btnLike-${postObject.id}" class="border-box btn-icon-post bg-green" src="../assets/heart.png" alt="likes" title="likes" />
 				  ${(user && user.uid === postObject.userId) ? `<img id="btn-edit-${postObject.id}" class="border-box btn-icon btn-icon-post bg-green" src="../assets/paper-plane.png" alt="editar-post" />`: ''}
 				  ${(user && user.uid === postObject.userId) ? `<select id="edit-privacy-${postObject.id}" class="select bg-green color-white border-none hide" disabled="true">
@@ -164,9 +164,9 @@ export const postListTemplate = (postObject) => {
 	getAllLikesPost(postObject.id, (likes) => {
     	const likesCounter = likes.length;
     	const likesSpan = article.querySelector(`#likes-count-${postObject.id}`);
-    	likesSpan.innerHTML = likesCounter;
     	userLikes = likes.find((comment) => comment.userName === user.email);
-    	(userLikes !== undefined) ? btnLike.src = '../assets/heart.png' : btnLike.src = '../assets/heart-empty.png';
+		(userLikes !== undefined) ? btnLike.src = '../assets/heart.png' : btnLike.src = '../assets/heart-empty.png';
+			likesSpan.innerHTML = (userLikes !== undefined) ?  (likesCounter===1) ?  `a ti te encanta` :  `a ti y a ${likesCounter -1} personas le encanta` : (likesCounter===0)? "":`a ${likesCounter} personas le encanta`;
     });
 
     btnLike.addEventListener('click', () => {
@@ -176,8 +176,8 @@ export const postListTemplate = (postObject) => {
     	  	btnLike.src = '../assets/heart.png';
     	    const likesCounter = likes.length;
     	    const likesSpan = article.querySelector(`#likes-count-${postObject.id}`);
-    	    likesSpan.innerHTML = likesCounter;
-    	    userLikes = likes.find((comment) => comment.userName === user.email);
+			userLikes = likes.find((comment) => comment.userName === user.email);
+			likesSpan.innerHTML = likesCounter===1 ?  `a ti te encanta` :  `a ti y a ${likesCounter -1} personas le encanta`;
             })
     	  );	
     	}  else {
@@ -186,7 +186,7 @@ export const postListTemplate = (postObject) => {
     	  	btnLike.src = '../assets/heart-empty.png';
     	    const likesCounter = likes.length;
     	    const likesSpan = article.querySelector(`#likes-count-${postObject.id}`);
-    	    likesSpan.innerHTML = likesCounter;
+    	    likesSpan.innerHTML =likesCounter===0 ? "":`a ${likesCounter} personas le encanta`;
     	    userLikes = undefined;
             })
     	  );
